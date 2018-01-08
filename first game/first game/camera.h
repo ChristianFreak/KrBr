287f2abf2941b03cd8a9d1cb3e6c71e1b0b0b6cd
BuildTools <unconfigured@null.spigotmc.org>
2018-01-08 21:40:42 +0100

    Test Meshes

--- first game/first game/camera.h
+++ first game/first game/camera.h
@@ -1,27 +1,65 @@
-#ifndef CAMERA_INCLUDED_H
-#define CAMERA_INCLUDED_H
+#ifndef CAMERA_H
+#define CAMERA_H
 
-#include "move.h"
+#include <glm/glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/transform.hpp>
 
-struct Camera : public Move
+using namespace glm;
+
+struct Camera
 {
+
 public:
-	Camera(const glm::vec3& pos, float fov, float aspect, float zNear, float zFar)
+	Camera(const vec3& pos, float fov, float aspect, float zNear, float zFar)
 	{
-		SetPos(pos);
-		SetForward(glm::vec3(0.0f, 0.0f, 1.0f));
-		SetChange(glm::vec3(0.0f, 0.0f, 1.0f));
-		SetUp(glm::vec3(0.0f, 1.0f, 0.0f));
-		projection = glm::perspective(fov, aspect, zNear, zFar);
+		m_position = pos;
+		m_forward    = vec3(0.0f, 0.0f, 1.0f);
+		m_up		 = vec3(0.0f, 1.0f, 0.0f);
+		m_projection = perspective(fov, aspect, zNear, zFar);
 	}
 
-	inline glm::mat4 GetViewProjection() const
+	inline mat4 GetViewProjection() const
 	{
-		return projection * GetView();
+		return m_projection * lookAt(m_position, m_position + m_forward, m_up);
 	}
+
+	void MoveForward(float amt)
+	{
+		m_position += m_forward * amt;
+	}
+
+	void MoveRight(float amt)
+	{
+		m_position -= cross(m_up, m_forward) * amt;
+	}
+
+	void Pitch(float angle)
+	{
+		vec3 right = normalize(cross(m_up, m_forward));
+
+		m_forward = vec3(normalize(rotate(angle, right) * vec4(m_forward, 0.0)));
+		m_up = normalize(cross(m_forward, right));
+	}
+
+	void RotateY(float angle)
+	{
+		static const vec3 UP(0.0f, 1.0f, 0.0f);
+
+		mat4 rotation = rotate(angle, UP);
+
+		m_forward = vec3(normalize(rotation * vec4(m_forward, 0.0)));
+		m_up = vec3(normalize(rotation * vec4(m_up, 0.0)));
+	}
+
 protected:
+
 private:
-	glm::mat4 projection;
+	mat4 m_projection;
+	vec3 m_position;
+	vec3 m_forward;
+	vec3 m_up;
+
 };
 
-#endif
+#endif //!CAMERA_H
--- first game/first game/main.cpp
+++ first game/first game/main.cpp
@@ -13,16 +13,27 @@ static const int DISPLAY_HEIGHT = 600;
 int main(int argc, char** argv)
 {
 	Display display(DISPLAY_WIDTH, DISPLAY_HEIGHT, "OpenGL");
+	 
+	Vertex vertices[] = 
+	{
+		Vertex(-1, 0.6f,-1,  1, 0,  0, 0, -1),
+		Vertex(-1, 0.3f, 1,  0, 0,  0, 0, -1),
+		Vertex( 1, 0.1f, 1,  0, 1,  0, 0, -1),
+		Vertex( 1, 0.3f,-1,  1, 1,  0, 0, -1),
+	};
 
-	Mesh monkey(".\\models\\monkey3.obj");
+	unsigned int indices[] = { 0, 1, 2, 0, 2, 3 };
+
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]), indices, sizeof(indices) / sizeof(indices[0]));
 	Shader shader(".\\shaders\\basicShader");
 	Texture texture(".\\textures\\tex_02.png");
 	Transform transform;
-	Camera camera(glm::vec3(0.0f, 0.0f, -5.0f), 70.0f, (float)DISPLAY_WIDTH / (float)DISPLAY_HEIGHT, 0.1f, 100.0f);
+
+	Camera camera(glm::vec3(0.0f, 2.0f, -5.0f), 70.0f, (float)DISPLAY_WIDTH / (float)DISPLAY_HEIGHT, 0.1f, 100.0f);
 
 	SDL_Event e;
 	bool isRunning = true;
-	float counter = 0.0f;
+
 	while (isRunning)
 	{
 		while (SDL_PollEvent(&e))
@@ -31,6 +42,36 @@ int main(int argc, char** argv)
 			{
 				isRunning = false;
 			}
+			if (e.type == SDL_KEYDOWN)
+			{
+				switch (e.key.keysym.sym)
+				{
+				case SDLK_w:
+					camera.MoveForward(0.1f);
+					break;
+				case SDLK_a:
+					camera.MoveRight(-0.1f);
+					break;
+				case SDLK_s:
+					camera.MoveForward(-0.1f);
+					break;
+				case SDLK_d:
+					camera.MoveRight(0.1f);
+					break;
+				case SDLK_UP:
+					camera.Pitch(-0.05);
+					break;
+				case SDLK_DOWN:
+					camera.Pitch(0.05);
+					break;
+				case SDLK_LEFT:
+					camera.RotateY(0.05);
+					break;
+				case SDLK_RIGHT:
+					camera.RotateY(-0.05);
+					break;
+				}
+			}
 		}
 
 		display.Clear(0.0f, 0.0f, 0.0f, 1.0f);
@@ -38,11 +79,10 @@ int main(int argc, char** argv)
 		shader.Bind();
 		texture.Bind();
 		shader.Update(transform, camera);
-		monkey.Draw();
+		mesh.Draw();
 
 		display.SwapBuffers();
 		SDL_Delay(1);
-		counter += 0.0001f;
 	}
 
 	return 0;
